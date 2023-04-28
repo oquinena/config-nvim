018ca8d8824a4251af1c5b34b7b7dc8910324566
Daniel de Oquiñena <daniel.oquinena@safespring.com>
2023-04-28 15:46:12 +0200

    Adding buffer_manager

--- lua/oquinena/init.lua
+++ lua/oquinena/init.lua
@@ -2,6 +2,7 @@ require("oquinena.config")
 require("oquinena.keymap")
 require("oquinena.packer")
 require("oquinena.lualine")
+require("oquinena.buffer_manager")
 -- require("oquinena.tabnine")
 
 
--- lua/oquinena/packer.lua
+++ lua/oquinena/packer.lua
@@ -8,8 +8,13 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
 	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  
+  use {
+	  'j-morano/buffer_manager.nvim',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 

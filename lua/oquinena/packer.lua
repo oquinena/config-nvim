0f03b8bae520f8a81f11a2a2fcd12dafdb7bb498
Daniel de Oquiñena <daniel.oquinena@safespring.com>
2023-10-12 20:28:21 +0200

    Remove tagged version

--- lua/oquinena/packer.lua
+++ lua/oquinena/packer.lua
@@ -9,7 +9,6 @@ return require("packer").startup(function(use)
 
     use({
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.1",
         requires = { { "nvim-lua/plenary.nvim" } },
     })
 
